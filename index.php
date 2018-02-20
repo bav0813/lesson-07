2766be8526edae54d7309cfb1bc140365a44a543
Andrey Bychkovsky <bav0813@gmail.com>
2018-02-20 20:39:09 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,62 @@
+<?php
+    /**
+     * Created by PhpStorm.
+     * User: andrey
+     * Date: 19.02.18
+     * Time: 18:30
+     */
+
+
+
+    //Написать функцию, которая будет принимать в качестве аргумента не пустой массив,
+    // будет находить минимальное и максимальнрое значения массива и будет возвращать
+    // массив найденых ['min'=>$min, 'max' => $max] значений.
+
+   // Написать функцию, которая будет возвращать текст анонса. Она должна принимать 2 аргумента:
+    // строку текста, и количество символов. Если строка текста длиннее 2-го аргумента, то ее необходимо
+    // обрезать по границе ближайшего слова и добавить ... иначе возвращать строку без изменений.
+    function minmax(array $arr1){
+
+        $val1=min($arr1);
+        $val2=max($arr1);
+
+
+    $arr=array(
+        'min'=> $val1,
+        'max'=>$val2);
+    return $arr;
+    }
+
+    function announce($text, $chars){
+
+        if (strlen($text)>$chars) {
+
+            $arr = preg_split('//u',$text,-1,PREG_SPLIT_NO_EMPTY);
+            $i=$chars-1;
+            while ($i>=0) {
+                if ($arr[$i] <>' ') {
+                   $i--;
+                }
+                else {
+
+                    array_splice($arr,$i+1);
+                    $out=implode($arr);
+                    break;
+
+                }
+
+            $out='cannot split';
+            }
+        }
+        else {
+            $out=$text;}
+
+        return $out.'...';
+    }
+
+    //min-max
+    echo "output " .var_export(minmax([3,2,1,0,15,6]),1)."<br>";
+
+    //announce
+    $ann=announce('Количество символов, по которым строка будет перенесена',24);
+    echo "Announce: '$ann'";
